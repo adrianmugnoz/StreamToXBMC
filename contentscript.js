831871ff1eebb72de71d298e826c3b350a77643f
Adrian <adrianmugnoz@gmail.com>
2014-04-20 15:09:03 +0200

    Added vk, streamcloud, vidspot. Fixed allmyvideos.

--- contentscript.js
+++ contentscript.js
@@ -12,25 +12,67 @@ var playedto = /played/;
 var megafiles = /megafiles/;
 var bestreams = /bestreams/;
 var cbs = /cbs/;
+var vk = /vk/;
+var streamcloud = /streamcloud/;
+var vidspot = /vidspot/;
+var magnovideo = /magnovideo/;
 
-if(allmyv.test(url))
-    regex = /"file" : "(.+?)"/;
-else if(vidto.test(url))
+
+if(allmyv.test(url)){
+    regex= /"file" : "(.+?)",/;
+    
+    
+}
+else if(vidto.test(url)){
     regex = /file_link = '(.+?)'/;
-else if(playedto.test(url))
-    regex = /file: "(.+?)"/
-else if(megafiles.test(url))
-    regex = /file_link = '(.+?)'/
-else if(bestreams.test(url))
-    regex = /file: "(.+?)"/
-else if(cbs.test(url))
-    regex = /video.settings.pid = '(.*?)';/
+}
+else if(playedto.test(url)){
+    regex = /file: "(.+?)"/;
+}
+else if(megafiles.test(url)){
+    regex = /file_link = '(.+?)'/;
+}
+else if(bestreams.test(url)){
+    regex = /file: "(.+?)"/;
+}
+else if(cbs.test(url)){
+    regex = /video.settings.pid = '(.*?)'/;
+}
+else if(vk.test(url)){
+    regex720 = /"url720":"(.+?)"/;
+    regex480 = /"url480":"(.+?)"/;
+    regex = /"url360":"(.+?)"/;
+    
+}    
+else if(streamcloud.test(url)){
+    regex = /file: "(.+?)"/;
+}
+else if(vidspot.test(url)){
+    regex = /"file" : "(.+?)",/;
+}
+else if(magnovideo.test(url)){
+    regex = /swf?flv=(.+?)/;
+}
+
 
 
 // Test the text of the body element against our regular expression.
-    if (regex.test(document.body.innerHTML)) {
+if(vk.test(url)){
+    if (regex720.test(document.body.innerHTML)) {
       // The regular expression produced a match, so notify the background page.
+      chrome.extension.sendRequest({msg:regex720.exec(document.body.innerHTML), url:url}, function(response) {});
+    } else if(regex480.test(document.body.innerHTML)){
+      chrome.extension.sendRequest({msg:regex480.exec(document.body.innerHTML), url:url}, function(response) {});
+    } else if(regex.test(document.body.innerHTML)){
       chrome.extension.sendRequest({msg:regex.exec(document.body.innerHTML), url:url}, function(response) {});
-    } else {
-      // No match was found.
     }
+
+} else{
+
+    if(regex.test(document.body.innerHTML)){
+      chrome.extension.sendRequest({msg:regex.exec(document.body.innerHTML), url:url}, function(response) {});
+    }
+}    
+
+
+
